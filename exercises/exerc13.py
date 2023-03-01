51728ecaa4fdac0dbcbcf6f99a727effa88f4743
Ralf <r@binder.contact>
2023-03-01 17:43:32 +0700

    Abschnitt 15 komplett abgeschlossen

---
+++ exercises/exerc13.py
@@ -0,0 +1,7 @@
+import random
+
+lower_b = int(input("Enter the lower bound: "))
+upper_b = int(input("Enter the upper bound: "))
+
+random_number = random.randint(lower_b, upper_b)
+print(random_number)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # from functions import get_todos, write_todos
 import functions
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print(f"It is {now}")
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
