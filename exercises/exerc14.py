ccdec0b79401e033f2a91182ca4114894fa9a761
binder <r@binder.contact>
2023-03-06 14:49:09 +0700

    Implement complete and exit button

--- exercises/exerc14.py
+++ exercises/exerc14.py
@@ -1,17 +1,25 @@
 import PySimpleGUI as sg
+from converters8 import convert
 
 feet_label = sg.Text("Enter feet:")
-feet_input = sg.Input()
+feet_input = sg.Input(key="feets")
 
 inches_label = sg.Text("Enter inches:")
-inches_input = sg.Input()
+inches_input = sg.Input(key="inches")
 
 button = sg.Button("Convert")
+result_label = sg.Text(key="result")
 
 window = sg.Window("Converter",
                    layout=[[feet_label, feet_input],
                            [inches_label, inches_input],
-                           [button]])
+                           [button, result_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    feets = float(values["feets"])
+    inches = float(values["inches"])
+    meters = convert(feets, inches)
+    window["result"].update(value=f"{meters:.3f} m")
+
 window.close()
